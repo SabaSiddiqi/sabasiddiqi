04975b8feca81bd722926fb9c7e9749b61408acf
SabaSiddiqi <sabasiddiqi098@hotmail.com>
2018-12-10 01:39:30 -0500

    main page css edited

--- sabasiddiqi/urls.py
+++ sabasiddiqi/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -13,3 +15,8 @@ urlpatterns = [
     path('home/', include('home.urls',namespace='home')),
 
 ]
+
+
+#https://simpleisbetterthancomplex.com/tutorial/2016/08/01/how-to-upload-files-with-django.html
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
